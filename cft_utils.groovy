cca4b00673238a3438fbebace4fd658291dae7b2
mani1263 <manikandaninspire@gmail.com>
2026-01-06 20:25:13 +0530

    Update cft_utils.groovy

--- cft_utils.groovy
+++ cft_utils.groovy
@@ -1,28 +1,34 @@
+// cft_utils.groovy
+
 def validateTemplate(stackName, templateFile, region = "us-east-1") {
     sh """#!/usr/bin/env bash
     set -e
     aws --region ${region} cloudformation validate-template \
         --template-body file:///${templateFile}
     """
-}
+    }
 
 def createChangeSet(stackName, templateFile, region = "us-east-1") {
-    sh """#!/usr/bin/env 
-        bash set -e 
-            aws --region ${region} cloudformation create-change-set \
-                --stack-name ${stackName} \
-                --change-set-name ${stackName}-changeset \
-                --template-body file:///${templateFile} \
-                --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM """ }
-
+    sh """#!/usr/bin/env bash
+    set -e
+    aws --region ${region} cloudformation create-change-set \
+        --stack-name ${stackName} \
+        --change-set-name ${stackName}-changeset \
+        --template-body file:///${templateFile} \
+        --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM
+    """
+    }
 
 def updateStack(stackName, templateFile, region = "us-east-1") {
-    sh """#!/usr/bin/env 
-        bash set -e 
-        aws --region ${region} cloudformation deploy \
-            --stack-name ${stackName} \
-            --template-file /home/ubuntu/devel/inspire-tools/cloudformation/${stackName}.yml \
-            --no-fail-on-empty-changeset \
-            --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM """
-                                                  
+    sh """#!/usr/bin/env bash
+    set -e
+    aws --region ${region} cloudformation deploy \
+        --stack-name ${stackName} \
+        --template-file ${templateFile} \
+        --no-fail-on-empty-changeset \
+        --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM
+    """
+    }
+
+// MUST be at the very end so `load` works
 return this
