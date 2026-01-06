b12f3b0d3f63b2335fc21b704655bf3a9349f929
mani1263 <manikandaninspire@gmail.com>
2026-01-06 20:06:20 +0530

    Rename templatePath to templateFile in validateTemplate

--- cft_utils.groovy
+++ cft_utils.groovy
@@ -1,8 +1,8 @@
-def validateTemplate(stackName, templatePath, region = "us-east-1") {
+def validateTemplate(stackName, templateFile, region = "us-east-1") {
     sh """#!/usr/bin/env bash
     set -e
     aws --region ${region} cloudformation validate-template \
-        --template-body file:///${templatePath}.yml
+        --template-body file:///${templateFile}
     """
 }
 
