6d626ea432870f6010a6542b9122a12a6d4a72f2
mani1263 <manikandaninspire@gmail.com>
2026-01-06 20:23:56 +0530

    Update cft_utils.groovy

--- cft_utils.groovy
+++ cft_utils.groovy
@@ -15,4 +15,14 @@ def createChangeSet(stackName, templateFile, region = "us-east-1") {
                 --template-body file:///${templateFile} \
                 --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM """ }
 
+
+def updateStack(stackName, templateFile, region = "us-east-1") {
+    sh """#!/usr/bin/env 
+        bash set -e 
+        aws --region ${region} cloudformation deploy \
+            --stack-name ${stackName} \
+            --template-file /home/ubuntu/devel/inspire-tools/cloudformation/${stackName}.yml \
+            --no-fail-on-empty-changeset \
+            --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM """
+                                                  
 return this
