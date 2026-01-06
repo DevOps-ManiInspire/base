a662f46a5b313df75edf47370ba3b77f8f00777c
mani1263 <manikandaninspire@gmail.com>
2026-01-06 20:13:13 +0530

    Fix formatting in createChangeSet function

--- cft_utils.groovy
+++ cft_utils.groovy
@@ -5,13 +5,14 @@ def validateTemplate(stackName, templateFile, region = "us-east-1") {
         --template-body file:///${templateFile}
     """
 }
+
 def createChangeSet(stackName, region = "us-east-1") { 
     sh """#!/usr/bin/env 
         bash set -e 
-            aws --region ${region} cloudformation create-change-set \ 
-                --stack-name ${stackName} \ 
-                --change-set-name ${stackName}-changeset \ 
-                --template-body file:///${templateFile} \ 
+            aws --region ${region} cloudformation create-change-set \
+                --stack-name ${stackName} \
+                --change-set-name ${stackName}-changeset \
+                --template-body file:///${templateFile} \
                 --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM """ }
 
 return this
