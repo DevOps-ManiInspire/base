52e3eb08c6d987cedfa14fb04626e2398a020507
mani1263 <manikandaninspire@gmail.com>
2026-01-09 20:07:56 +0530

    Update cft_utils.groovy

--- cft_utils.groovy
+++ cft_utils.groovy
@@ -4,7 +4,7 @@ def validateTemplate(stackName, templateFile, region = "us-east-1") {
     sh """#!/usr/bin/env bash
     set -e
     aws --region ${region} cloudformation validate-template \
-        --template-body file:///${templateFile}
+        --template-file ${templateFile}
     """
     }
 
@@ -14,7 +14,7 @@ def createChangeSet(stackName, templateFile, region = "us-east-1") {
     aws --region ${region} cloudformation create-change-set \
         --stack-name ${stackName} \
         --change-set-name ${stackName}-changeset \
-        --template-body file:///${templateFile} \
+        --template-file ${templateFile} \
         --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM
     """
     }
